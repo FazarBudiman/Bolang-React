a21304772b7ea9c05762acabf414b1041517be1a
Fazar Budiman <fazarbudiman1907@gmail.com>
2023-12-20 18:15:59 +0700

    change router

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,12 +1,19 @@
 import { Button, Space } from "antd";
 import { ContainerOutlined, HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const BottomNav = () => {
   return (
     <Space className="bottom-nav">
-      <Button href="/dashboard" type="text" icon={<HomeOutlined />} size="large"></Button>
-      <Button href="/history" type="text" icon={<ContainerOutlined />} size="large"></Button>
-      <Button href="/profile" type="text" icon={<UserOutlined />} size="large"></Button>
+      <Link to="/dashboard">
+        <Button type="text" icon={<HomeOutlined />} size="large"></Button>
+      </Link>
+      <Link to="/history">
+        <Button type="text" icon={<ContainerOutlined />} size="large"></Button>
+      </Link>
+      <Link to="/profile">
+        <Button type="text" icon={<UserOutlined />} size="large"></Button>
+      </Link>
     </Space>
   );
 };
